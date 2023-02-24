4fe5ab6597cbc808bda324909733fe63e8276328
Pooran Mal Meena <pooranmal@appnwebtechnologies.co.in>
2023-02-24 10:29:48 +0530

    no message

---
+++ lib/Controller/ProjectController/MainController/import_controller.dart
@@ -0,0 +1,76 @@
+
+
+/// Main
+export '../../../main.dart';
+
+
+/// Packages
+
+
+/// GetX Controller
+export '../../ProjectController/DriverProfile/driverProfileController.dart';
+export '../../ProjectController/LoginController/login_controller.dart';
+export '../../ProjectController/MainController/main_controller.dart';
+export '../../ProjectController/NotificationController/notificationController.dart';
+export '../../ProjectController/SetupPinController/setupPinController.dart';
+export '../../ProjectController/Splash/splash_controller.dart';
+
+
+/// Controller
+export '../../Helper/ColorExtention/ColorExtention.dart';
+export '../../Helper/ApplicationExit/AppExitValidator.dart';
+export '../../Helper/AppVersion/AppVersion.dart';
+export '../../Helper/Camera/CameraScreen.dart';
+export '../../Helper/ConnectionValidator/ConnectionValidator.dart';
+export '../../Helper/DeviceInfo/DeviceInfo.dart';
+export '../../Helper/Dimensions/Dimensions.dart';
+export '../../Helper/DownloadFile/DownloadFileCustom.dart';
+export '../../Helper/DrawCircle/drawCircle.dart';
+export '../../Helper/FormFieldValidator/formFieldValidator.dart';
+export '../../Helper/ImagePicker/ImagePicker.dart';
+export '../../Helper/Permission/PermissionHandler.dart';
+export '../../Helper/PrintLog/PrintLog.dart';
+export '../../Helper/Shared Preferences/SharedPreferences.dart';
+export '../../Helper/TextController/BuildText/BuildText.dart';
+export '../../Helper/TextController/FontFamily/FontFamily.dart';
+export '../../Helper/TextController/FontStyle/FontStyle.dart';
+export '../../Helper/TextController/TextValidator/TextValidator.dart';
+export '../../Helper/TouchAndFaceOverlay/touchAndFaceOverlay.dart';
+export '../../ApiController/ApiController.dart';
+export '../../ApiController/WebConstant.dart';
+export '../../Firebase/FirebaseDynamicLink/FirebaseDynamicLink.dart';
+export '../../Firebase/FirebaseMessaging/FirebaseMessaging.dart';
+export '../../RouteController/Route.dart';
+export '../../RouteController/RouteNames.dart';
+
+/// Widget Controller
+export '../../WidgetController/AdditionalWidget/CustomAppBar.dart';
+export '../../WidgetController/AdditionalWidget/ExpansionTileCard/expansionTileCardWidget.dart';
+export '../../WidgetController/BottomSheet/BottomSheetCustom.dart';
+export '../../WidgetController/Button/ButtonCustom.dart';
+export '../../WidgetController/CustomDrawer/drawerDriver.dart';
+export '../../WidgetController/Default Widget/DefaultWidget.dart';
+export '../../WidgetController/ErrorHandling/EmptyDataScreen.dart';
+export '../../WidgetController/ImageHelper/ImageHelper.dart';
+export '../../WidgetController/Loader/LoadingScreen.dart';
+export '../../WidgetController/Popup/PopupCustom.dart';
+export '../../WidgetController/Popup/CustomDialogBox.dart';
+export '../../WidgetController/Popup/CustomAddReviewDialog.dart';
+export '../../WidgetController/RefresherIndicator/RefreshIndicatorCustom.dart';
+export '../../WidgetController/TextField/CustomTextField.dart';
+export '../../WidgetController/Toast/ToastCustom.dart';
+
+
+
+/// Model
+
+
+
+/// Screen's
+
+
+
+/// Asset's
+export '../../Helper/StringDefine/StringDefine.dart';
+export '../../Helper/Colors/custom_color.dart';
+
--- lib/View/OnBoarding/Splash/splash_screen.dart
+++ lib/View/OnBoarding/Splash/splash_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pharmdel/Controller/Helper/Colors/custom_color.dart';
-import 'package:pharmdel/Controller/Helper/TextController/BuildText/BuildText.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../../Controller/Helper/StringDefine/StringDefine.dart';
+import '../../../Controller/Helper/TextController/BuildText/BuildText.dart';
 import '../../../Controller/ProjectController/Splash/splash_controller.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,11 +12,9 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-SplashController splashController = SplashController();
 
 class _SplashScreenState extends State<SplashScreen> {
-  final formKey = GlobalKey<FormState>();
-  final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
   SplashController splashCT = Get.put(SplashController());
 
   @override
@@ -27,29 +23,21 @@ class _SplashScreenState extends State<SplashScreen> {
         init: splashCT,
         builder: (controller) {
           return Scaffold(
-              appBar: null,
-              key: scaffoldKey,
               body: Center(
                 child: SafeArea(
                     child: SingleChildScrollView(
                         child: Center(
-                            child: Column(
-                  children: [
-                    Image.asset(
-                      strimg_logo,
-                      height: 200,
-                      width: 200,
-                    ),
-                    const SizedBox(height: 80),
-                   CircularProgressIndicator(color: AppColors.colorAccent,)
-                  ],
-                )))),
+                          child: Column(
+                              children: [
+                                Image.asset(strimg_logo,height: 200,width: 200,),
+                                buildSizeBox(80.0,0.0),
+                                CircularProgressIndicator(color: AppColors.colorAccent,)
+                              ],
+                          )
+                        )
+                    )
+                ),
               ));
         });
   }
-  Future<void> setLastTime() async {
-  final prefs = await SharedPreferences.getInstance();
-  DateTime now = DateTime.now();
-  // prefs.setInt(WebConstant.USER_LASTTIME, now.millisecondsSinceEpoch);
-}
 }
